f7a8b65896db5d7329045db0e8f3bdf7e18f5ebb
Иван Перепелицин <perepelitsin.ivan@gmail.com>
2023-09-22 01:30:54 +0700

    task1 ver1

---
+++ task1/src/main/java/ru/cft/focus/Main.java
@@ -0,0 +1,8 @@
+package ru.cft.focus;
+
+public class Main {
+    public static void main(String[] args) {
+        MultiplicationTable mt = new MultiplicationTable();
+        mt.input();
+    }
+}
